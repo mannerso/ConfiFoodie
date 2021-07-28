f2b3dbc542e7b6ef56e35fb96d000cb6b4615b6b
Samantha Peterson <85209802+SamanthaPeterson@users.noreply.github.com>
2021-07-28 13:08:39 -0600

    changes to be submitted for weather

--- weather.js
+++ weather.js
@@ -1,34 +1,38 @@
 var weather;
 
- var api = 'http://api.openweathermap.org/data/2.5/weather?q=' ;
- var city = 'Salt Lake City';
- var apiKey = '&appid=d196b19dfff57ff2ede0751c52d064e0';
- var units = '&units=imperial';
-function setup() {
-    createCanvas(400,200) ;
-    var button = select ('#submit');
-    button.mousePressed(findWeather);
+var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = '&appid=d196b19dfff57ff2ede0751c52d064e0';
+var units = '&units=imperial';
+var input;
 
-} 
-   document.getElementById('submit').addEventListener ('click', findWeather());
+function setup() {
+  createCanvas(400, 200);
+
+  var button = select('#submit');
+  button.mousePressed(findWeather);
+
+  input = select('#city');
+
+}
+document.getElementById('submit').addEventListener('click', findWeather());
 
 function findWeather() {
-    var url= api + city + apiKey + units;
-    fetch(
-        url
-      )
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          console.log(data);
-        });
-      
-    console.log (url)
-    // loadJSON (url, gotData) ;
- 
-}
+  var url = api + input.value() + apiKey + units;
 
-function gotData(data) {
+  //     fetch(
+  //         url
+  //       )
+  //         .then(function(response) {
+  //           return response.json();
+  //         })
+  //         .then(function(data) {
+  //           console.log(data);
+  //         });
+
+  //     console.log (url)
+  //     // loadJSON (url, gotData) ;
+
+  // }
+
+  function gotData(data) {
     weather = data;
-}
