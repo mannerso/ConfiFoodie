e412d71fe98f82e5aa10e6b82eea6f9064450f3a
Leslie Codes <84750526+Lesliescodes@users.noreply.github.com>
2021-07-31 14:25:12 -0600

    final commit

--- Scripts/weather.js
+++ Scripts/weather.js
@@ -1,37 +1,38 @@
 // var weather;
 
- var api = 'http://api.openweathermap.org/data/2.5/weather?q=' ;
- var apiKey = '&appid=d196b19dfff57ff2ede0751c52d064e0';
- var units = '&units=imperial';
- var input;
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = '&appid=d196b19dfff57ff2ede0751c52d064e0';
+var units = '&units=imperial';
+var input = document.querySelector("#city");
+var searchField = document.querySelector("#searchField");
 
- function setup() {
+function setup() {
 
-    var button = select ('#submit');
-    button.mousePressed(findWeather);
-
-     <input=select('#city')> <input>
-
-} 
-   document.getElementById('submit').addEventListener ('click', findWeather());
-
-function findWeather() {
-    var url= api + input.value() + apiKey + units;
+  var button = select('#submit');
+  button.mousePressed(findWeather);
+  searchField.addEventListener("submit", function (event) {
+    event.preventDefault();
+  })
 }
-    fetch(
-      'http.api.openweathermap.org/data/2.5/weather?q='
-      ,
-        then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          console.log(url);
-        }
-    
-    // loadJSON (url, gotData) ;
- 
-}
+//  <input=select:true ('#city')> <input>
 
-function gotData(data) {
-    weather = data;
+document.getElementById('searchButton').addEventListener('click', findWeather)
+
+function findWeather(event) {
+  event.preventDefault();
+  input = input.value;
+  url = url + input + apiKey + units;
+  console.log(url);
+  fetch(url)
+    // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}    
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.main.temp);
+      var temp = data.main.temp
+      var tempWeather = document.querySelector('#weatherTemp');
+      tempWeather.innerText = temp;
+
+
+
+    })
 }
